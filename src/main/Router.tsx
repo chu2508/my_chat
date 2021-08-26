19b75ed0a587f0bc124a9bfeb73ebce2de84c2dc
dirac <767848898@qq.com>
2021-08-26 18:32:25 +0800

    add sign up page in router config

--- src/main/Router.tsx
+++ src/main/Router.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { ComponentType, lazy, Suspense } from "react";
 import { Switch, Route, useLocation, Redirect } from "react-router";
 import SignIn from "../views/SignIn";
+
+const SignUp = lazy(() => import("../views/SignUp"));
+
+const withSuspense =
+  (WrapComponent: ComponentType): ComponentType =>
+  (props: any) => {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <WrapComponent {...props} />
+      </Suspense>
+    );
+  };
 
 const Router = () => {
   const location = useLocation();
   return (
     <Switch location={location}>
-      <Redirect exact path="/" to="/sign_in"/>
+      <Redirect exact path="/" to="/sign_in" />
       <Route path="/sign_in" component={SignIn} />
+      <Route path="/sign_up" component={withSuspense(SignUp)} />
     </Switch>
   );
 };
