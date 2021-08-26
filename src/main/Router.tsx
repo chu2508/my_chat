86b5bb3fd5d448386ac5fe92f65f51665f0c5aae
dirac <767848898@qq.com>
2021-08-26 17:31:51 +0800

    change sign in page link path

--- src/main/Router.tsx
+++ src/main/Router.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Switch, Route, useLocation } from "react-router";
+import { Switch, Route, useLocation, Redirect } from "react-router";
 import SignIn from "../views/SignIn";
 
 const Router = () => {
   const location = useLocation();
   return (
     <Switch location={location}>
-      <Route path="/" component={SignIn} />
+      <Redirect exact path="/" to="/sign_in"/>
       <Route path="/sign_in" component={SignIn} />
     </Switch>
   );
--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/icons/logo.svg";
 import { Input } from "../../components/ui";
 import Button from "../../components/ui/Button";
+import { useHistory, useLocation } from 'react-router';
 
 const slogan = "My chat";
 const SignIn = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const gotoSingUp = () => {};
-  const submit = () => {};
+  const history = useHistory()
+  const gotoSingUp = () => {
+    history.push('/sign_up')
+  };
+  const submit = () => {
+    history.replace('/home')
+  };
   return (
     <div>
-      <img style={{width: 60, height: 60, margin: '0 auto', display: 'block'}} src={logo} alt="logo" />
+      <img style={{ width: 60, height: 60, margin: "0 auto", display: "block" }} src={logo} alt="logo" />
       <h1>{slogan}</h1>
       <Input value={username} onChange={(value) => setUsername(value)} placeholder="please enter your username" />
       <Input value={password} onChange={(value) => setPassword(value)} placeholder="Password" />
@@ -20,7 +27,7 @@ const SignIn = () => {
         <Button onClick={submit}>Sign in</Button>
       </div>
       <div>
-        <a href="/account/reset_password">lost password?</a>
+        <Link to="/lost_pwd">lost password?</Link>
       </div>
       <p>Power by XXXX</p>
     </div>
