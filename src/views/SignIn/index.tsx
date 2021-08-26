861712c0b82bc1e8fa5222fb45b63e160ac53b88
dirac <767848898@qq.com>
2021-08-26 16:25:40 +0800

    change project folder structure

--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
-import logo from "../../logo.svg";
-import {} from 'react-icons/bi'
+import logo from "../../assets/icons/logo.svg";
 import { Input } from "../../components/ui";
 import Button from "../../components/ui/Button";
 
-const slogan = 'My chat'
+const slogan = "My chat";
 const SignIn = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const gotoSingUp = () => {}
-  const submit = () => {}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const gotoSingUp = () => {};
+  const submit = () => {};
   return (
     <div>
       <img src={logo} alt="logo" />
       <h1>{slogan}</h1>
-      <Input value={username} onChange={(value) => setUsername(value)} placeholder="please enter your username"/>
-      <Input value={password} onChange={(value) => setPassword(value)} placeholder="Password"/>
+      <Input value={username} onChange={(value) => setUsername(value)} placeholder="please enter your username" />
+      <Input value={password} onChange={(value) => setPassword(value)} placeholder="Password" />
       <div>
         <Button onClick={gotoSingUp}>Sign up</Button>
         <Button onClick={submit}>Sign in</Button>
